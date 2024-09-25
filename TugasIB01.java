a1a908c1450f8b89077f49af93689e9c4bee4898
Abimmmm <156158397+Abimmmm@users.noreply.github.com>
2024-09-25 10:25:42 +0700

    Tugas jobsheet5 b

---
+++ TugasIB01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TugasIB01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int usia;
+        System.out.print("Masukan Usia Anda : ");
+        usia = sc.nextInt();
+
+        if (usia < 0) {
+            System.out.println("Usia tidak boleh negatif");
+        } else if (usia < 13) {
+            System.out.println("Remaja");
+        } else if (usia < 20) {
+            System.out.println("Dewasa");
+        } else if (usia < 65) {
+            System.out.println("Lansia");
+        } else {
+            System.out.println("Lansia");
+        }
+    }
+}
